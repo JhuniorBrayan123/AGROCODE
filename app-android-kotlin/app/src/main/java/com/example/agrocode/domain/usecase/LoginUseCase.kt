1b804ac9c9123f60efcb56b42a46cd441b85ed09
Kaquenduri <marchunque7880@gmail.com>
2025-09-28 21:05:06 -0500

    fix(UI): Login variable Error solucionada

--- app-android-kotlin/app/src/main/java/com/example/agrocode/domain/usecase/LoginUseCase.kt
+++ app-android-kotlin/app/src/main/java/com/example/agrocode/domain/usecase/LoginUseCase.kt
@@ -5,10 +5,10 @@ import com.example.agrocode.domain.model.Usuario
 
 interface LoginUseCase {
     suspend fun iniciarSesion(credenciales: CredencialesLogin): ResultadoLogin
-    data class Error(val mensaje: String) : ResultadoLogin()
 }
 
 sealed class ResultadoLogin {
     data class Exitoso(val usuario: Usuario) : ResultadoLogin()
+    data class Error(val mensaje: String) : ResultadoLogin()
     object Cargando : ResultadoLogin()
 }
